1d3bf0884ac26e44ce4195ee956266e13e6d51d4
anto95240 <richard33antoine@gmail.com>
2026-04-16 17:16:35 +0200

    feat: add raccourci

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import packageJson from './package.json'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
   define: {
     // Crée une variable globale avec la version
     'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version)
